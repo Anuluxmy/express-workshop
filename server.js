fae79e188027b9db5837a2fa2d99d6d3acae9c46
anuluxmy <anu_iv2002@hotmail.com>
2019-05-15 21:23:25 +0100

    html page

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.get("/node", function (req, res) {
-  res.send("Hello World!");
-});
+app.use(express.static("public"));
+
 
 app.listen(3000, function () {
   console.log('Server is listening on port 3000. Ready to accept requests!');
